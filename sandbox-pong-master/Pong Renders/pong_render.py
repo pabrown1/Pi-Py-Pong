ab36b1f074c0d4c6a0c8bd29c58f00777dac3ab2
pabrown1 <pabrown@unomaha.edu>
2014-08-01 10:34:03 -0500

    transparency fix
    attempt to convert image to display transparency

--- sandbox-pong-master/Pong Renders/pong_render.py
+++ sandbox-pong-master/Pong Renders/pong_render.py
@@ -97,6 +97,7 @@ if __name__ == '__main__':
         paddle_index = 2
     else:
 	    paddle = pygame.image.load( 'assets/blank.png' ).convert() # added to correct error with non-edge nodes
+		paddle.image.convert_alpha() # to make image transparent
         paddle_rect = paddle.get_rect()
         paddle_rect.x = 84
         edge_node = True
